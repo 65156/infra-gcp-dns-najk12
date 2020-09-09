df26d3e0f1ba7e1716d7f709f373e071377284be
Fraser Elliot Carter Smith <fraser.carter@gmail.com>
2020-09-09 11:17:45 +1000

    rebased files

--- provider.tf
+++ provider.tf
@@ -6,7 +6,9 @@ provider "google-beta" {
   region = var.global["default_resource_region"]
 }
 
-
-
-
-
+terraform {
+  backend "gcs" {
+    bucket = "statefiles-tf-xjdfh2"
+    prefix = "gcp/dns"
+  }
+}
